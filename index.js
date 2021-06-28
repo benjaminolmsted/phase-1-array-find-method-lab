24c6a493c36c2f2119fa06254f426e3a63bf784c
benjamin <benjamin.olmsted@gmail.com>
2021-06-28 16:29:17 -0700

    commit tree 6322c5a1ac7ee99dcd9924f7c14e53fe7f8cb699

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function didWin(element){
+    return element.result === 'W';
+}
 
+function superbowlWin(arr){
+    const team = arr.find(didWin);
+    if(team)    
+        return team.year;
+    return undefined;
+}
